562e2a0a028b01f37d222482127bf4e824f1000e
saikirananadhi <saikirananadhi@gmail.com>
2026-01-12 11:07:00 +0530

    Update lamda_function.py

--- hello_world/lamda_function.py
+++ hello_world/lamda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "SOP Reasoner1: Testing Version1..."
+        "body": "SOP Reasoner1: Testing Version2..."
     }
