a0d9827abc383a6d82df81604d6c3e853cbad792
saikirananadhi <saikirananadhi@gmail.com>
2026-01-13 10:44:31 +0530

    Update folders

--- hello_world/lamda_function.py
+++ hello_world/lamda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "SOP Reasoner1: Testing Version X..."
+        "body": "SOP Reasoner1: Version 1.0.1"
     }
